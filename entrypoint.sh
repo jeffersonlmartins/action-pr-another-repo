893275c5c6037d498bf3a77ede4d9dca4a92efdf
Jefferson L. Martins <jefferson.martins@solfacil.com.br>
2023-07-30 18:46:34 -0300

    change entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,55 +1,56 @@
 #!/bin/sh
+
 set -e
 set -x
 
-exit_with_error() {
-    echo "Error: $1"
-    exit 1
-}
-
-if [ -z "$INPUT_SOURCE_FOLDER" ]; then
-  exit_with_error "Source folder must be defined"
+if [ -z "$INPUT_SOURCE_FOLDER" ]
+then
+  echo "Source folder must be defined"
+  return -1
 fi
 
-if [ "$INPUT_DESTINATION_HEAD_BRANCH" = "main" ] || [ "$INPUT_DESTINATION_HEAD_BRANCH" = "master" ]; then
-  exit_with_error "Destination head branch cannot be 'main' nor 'master'"
+if [ $INPUT_DESTINATION_HEAD_BRANCH == "main" ] || [ $INPUT_DESTINATION_HEAD_BRANCH == "master"]
+then
+  echo "Destination head branch cannot be 'main' nor 'master'"
+  return -1
 fi
 
-# reviewers from pull request created
-PULL_REQUEST_REVIEWERS=""
-if [ -n "$INPUT_PULL_REQUEST_REVIEWERS" ]; then
-  PULL_REQUEST_REVIEWERS="-r $INPUT_PULL_REQUEST_REVIEWERS"
+if [ -z "$INPUT_PULL_REQUEST_REVIEWERS" ]
+then
+  PULL_REQUEST_REVIEWERS=$INPUT_PULL_REQUEST_REVIEWERS
+else
+  PULL_REQUEST_REVIEWERS='-r '$INPUT_PULL_REQUEST_REVIEWERS
 fi
 
 CLONE_DIR=$(mktemp -d)
 
+echo "Setting git variables"
 export GITHUB_TOKEN=$API_TOKEN_GITHUB
 git config --global user.email "$INPUT_USER_EMAIL"
 git config --global user.name "$INPUT_USER_NAME"
 
+echo "Cloning destination git repository"
 git clone "https://$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
 
-# Copy files to repository git
-cp -rf "$INPUT_SOURCE_FOLDER" "$CLONE_DIR/$INPUT_DESTINATION_FOLDER/"
-
+echo "Copying contents to git repo"
+mkdir -p $CLONE_DIR/$INPUT_DESTINATION_FOLDER/
+cp $INPUT_SOURCE_FOLDER "$CLONE_DIR/$INPUT_DESTINATION_FOLDER/"
 cd "$CLONE_DIR"
 git checkout -b "$INPUT_DESTINATION_HEAD_BRANCH"
 
+echo "Adding git commit"
 git add .
-
-if git status | grep -q "Changes to be committed"; then
-git commit --message "Update from https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
-
-# Push git commit
-git push -u origin HEAD:"$INPUT_DESTINATION_HEAD_BRANCH"
-
-# Create PR
-gh pr create -t "$INPUT_DESTINATION_HEAD_BRANCH" \
-                -b "$INPUT_DESTINATION_HEAD_BRANCH" \
-                -B "$INPUT_DESTINATION_BASE_BRANCH" \
-                -H "$INPUT_DESTINATION_HEAD_BRANCH" \
-                $PULL_REQUEST_REVIEWERS
+if git status | grep -q "Changes to be committed"
+then
+  git commit --message "Update from https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
+  echo "Pushing git commit"
+  git push -u origin HEAD:$INPUT_DESTINATION_HEAD_BRANCH
+  echo "Creating a pull request"
+  gh pr create -t $INPUT_DESTINATION_HEAD_BRANCH \
+               -b $INPUT_DESTINATION_HEAD_BRANCH \
+               -B $INPUT_DESTINATION_BASE_BRANCH \
+               -H $INPUT_DESTINATION_HEAD_BRANCH \
+                  $PULL_REQUEST_REVIEWERS
 else
-echo "No changes detected!"
+  echo "No changes detected"
 fi
-
